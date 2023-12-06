c7a21912763743e61e1b73c5d992e138fa3ccfe6
MAXAsbi <142154247+MAXAsbi@users.noreply.github.com>
2023-12-06 09:14:37 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -20,7 +20,7 @@ def get_svalue(val):
         if val == key:            
             return value
 def get_rvalue(val):    
-    feature_dict = {"South":1,"North":2,"West":3,"East":4}    
+    feature_dict = {"southwest":1,"northwest":2,"southeast":3,"northeast":4}    
     for key,value in feature_dict.items():        
         if val == key:            
             return value
@@ -30,51 +30,37 @@ def get_value(val,my_dict):
         if val == key:            
             return value
         
-app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction'])
+app_mode = st.sidebar.selectbox('Select Page',['Home','Graph'])
 if app_mode=='Home':
-    st.title('CAR DATASET')
-    # st.image('mobil.jpg')
-    st.header('Dataset: ')
+    st.title('INSURANCE PREDICTION')
     data = pd.read_csv('insurance.csv')
-    st.write(data)
     age = st.number_input('Input Your Age', 0,100)
     sex = st.radio('Set Your Gender', ['Male','Female'])
     bmi = st.number_input('Set Your BMI', 20,50)
     child = st.number_input('Input Your Children Count', 0, 10)
     smoker = st.radio('Are You A Smoker', ['Yes','No'])
-    region = st.radio('Select Your Region', ['South','North','West','East'])
+    region = st.radio('Select Your Region', ['Southwest','Northwest','Southeast','Northeast'])
     if st.button('Prediksi'):
-        # age_int = get_fvalue(sex)
         charges_pred = model.predict([[age, get_fvalue(sex), bmi, child, get_svalue(smoker), get_rvalue(region)]])
         charges_str = np.array(charges_pred)
         charges_float = float(charges_str[0])
 
         charges_formated = "{:.2f}".format(charges_float)
-        # if float(charges_formated) < 0:
-        #     st.write("MOBIL TIDAK DITEMUKAN")
-        # else:
         bar = st.progress(100)
         for percent_complete in range(100):
             time.sleep(0.01)
             bar.progress(percent_complete + 1)
         time.sleep(1)
         st.write("HASIL PREDIKSI BIAYA ASURANSI: $",charges_formated)
-# elif app_mode == 'Prediction': 
-#     st.title('CAR PRICE PREDICTION: ')
-#     horsepower = st.number_input('Set Horse Power', 48,288)
-#     highwaympg = st.number_input('Set Highway MPG', 16,54)
-#     curbweight = st.number_input('Set Curb Weight', 1488,4066)
-#     # cylinder = st.slider('Select Cylinder Number', 4,6)
-
-
-#     if st.button('Prediksi'):
-#         car_prediction = model.predict([[highwaympg, curbweight, horsepower]])
-
-#         harga_mobil_str = np.array(car_prediction)
-#         harga_mobil_float = float(harga_mobil_str[0])
-
-#         harga_mobil_formated = "{:.2f}".format(harga_mobil_float)
-#         if float(harga_mobil_formated) < 0:
-#             st.write("MOBIL TIDAK DITEMUKAN")
-#         else:
-#             st.write("HASIL PREDIKSI HARGA MOBIL: $",harga_mobil_formated)
+elif app_mode == 'Graph': 
+    data = pd.read_csv('insurance.csv')
+    st.header('Dataset: ')
+    st.write(data)
+    st.header('RATA RATA BIAYA BERDASARKAN REGION')
+    st.bar_chart(data['charges'].groupby(data.region).sum().sort_values(ascending = True))
+    st.header('PERBANDINGAN BIAYA PEROKOK dan BUKAN PEROKOK')
+    chart = alt.Chart(data).mark_circle().encode(
+    x='smoker',
+    y='charges',
+    ).interactive()
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
